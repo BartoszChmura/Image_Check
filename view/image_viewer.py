60923ca9027a0996ae0e13e3a670b36a1ec645f7
Bartosz Chmura <bartosz.chmura3@gmail.com>
2024-09-06 13:00:27 +0200

    Update: small improvements

--- view/image_viewer.py
+++ view/image_viewer.py
@@ -151,10 +151,32 @@ class ImageViewer(QMainWindow):
             self.current_index = len(self.image_list) - 1
             self.show_image()
 
+
     def close_app(self):
         if not self.user_initiated_close:
             self.user_initiated_close = True
             self.copy_checked_images_to_destination()
+
+        directories_to_clear = [
+            './zdjecia/do_sprawdzenia',
+            './zdjecia/nowe',
+            './zdjecia/sprawdzone',
+            './zdjecia/sylwetki'
+        ]
+
+        for directory in directories_to_clear:
+            if os.path.exists(directory):
+                for filename in os.listdir(directory):
+                    file_path = os.path.join(directory, filename)
+                    try:
+                        if os.path.isfile(file_path) or os.path.islink(file_path):
+                            os.unlink(file_path)
+                            print(f'Deleted file: {file_path}')
+                        elif os.path.isdir(file_path):
+                            shutil.rmtree(file_path)
+                    except Exception as e:
+                        print(f'Failed to delete {file_path}. Reason: {e}')
+
         self.close()
 
     def closeEvent(self, event):
